0db3d6629f87805c1b76b3e7a7627f02a151f505
Hemstreet <jonhemstreet@gmail.com>
2015-12-05 21:36:05 -0700

    code breakout

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var Cylon = require('cylon'),
     config = require('./config/config.json'),
     socket = require('socket.io-client')(config.url),
-    _ = require('underscore');
+    _ = require('underscore'),
+    relay = require('./lib/relay'),
+    lightShow = require('./lib/lightShow');
 
-// define the robot 
+// define the robot
 var robot = Cylon.robot({
     // change the port to the correct one for your Raspberry Pi
     rpi: null,
@@ -13,64 +15,42 @@ var robot = Cylon.robot({
     devices: {
         led: {driver: 'led', pin: 13}
     },
-    init: function (rpi) {
-        this.rpi = rpi;
+    work: function (rpi) {
+
+        relay.init({
+            rpi: rpi
+        });
+
+        lightShow.init({
+            rpi: rpi,
+            relay: relay
+        });
+
+        this.init({
+            rpi: rpi
+        });
+
+    },
+    init: function (options) {
+        this.rpi = options.rpi;
         this.isRunning = false;
+
         socket.on('connect', function () {
             console.log('connected');
         }.bind(this));
 
         // For testing purposes
         setTimeout(function () {
-            this.loadShow();
+            lightShow.load();
         }.bind(this), 3000);
 
         socket.on(config.bookEvent, function () {
-            this.toggleRelay(13, 1);
+            relay.toggle(13, 1);
 
             setTimeout(function () {
-                this.toggleRelay(13, 0);
+                relay.toggle(13, 0);
             }.bind(this), config.duration);
         }.bind(this));
-    },
-    toggleRelay: function (togglePin, value) {
-
-        console.log(togglePin, value);
-        if (value === 1) {
-            this.rpi.led.turnOn();
-        }
-        else {
-            this.rpi.led.turnOff();
-        }
-    },
-    lightShow: function () {
-        this.isRunning = true;
-
-
-        this.isRunning = false;
-    },
-    loadShow: function (name) {
-        var showName = name || "show1",
-            json = require('./lightShows/' + showName + '.json');
-
-        _.each(json.shows, function (value, _index) {
-            _.each(value.steps, function (milis, index) {
-                setTimeout(function () {
-
-                    var pin = value.pinNumber;
-                    if ((index + 1) % 2 == 0) {
-                        this.toggleRelay(pin, 0);
-                    }
-                    else {
-                        this.toggleRelay(pin, 1);
-                    }
-                }.bind(this), milis);
-            }.bind(this));
-        }.bind(this));
-    },
-    work: function (rpi) {
-
-        this.init(rpi);
 
     }
 });
